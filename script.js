20a9d4f79c390578d50e15b006ddca99bcd9a1bc
Arseniy Mirgorod <frailiht@gmail.com>
2020-06-05 19:34:47 +0300

    Второе задание выполнено

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 
 let money, income, addExpenses, deposit, mission, period;
 
-mission = 'hello js!';
-alert(mission);
-console.log("test");
+money = 50000;
+income = 10000;
+addExpenses = "интернет, такси, коммуналка";
+deposit = true;
+mission = 5000000;
+period = 12;
+
+console.log(typeof money );
+console.log(typeof income );
+console.log(typeof addExpenses);
+console.log(typeof deposit);
+console.log(typeof mission);
+console.log(typeof period);
