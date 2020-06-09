51a26ad2632764c83cdfe8de657eff43644856ad
Arseniy Mirgorod <frailiht@gmail.com>
2020-06-09 21:54:14 +0300

    Задание выполнено

--- script.js
+++ script.js
@@ -1,97 +1,19 @@
-let money, income, addExpenses, deposit, mission, period, expenses1, amount1, expenses2, amount2, accumulatedMonth;
-
-money = 60000;
-income = "фриланс";
-addExpenses = "интернет, такси, коммуналка";
-deposit = true;
-mission = 5000000;
-period = 12;
-
-let showTypeOf = function(data){
-    return typeof(data)
-}
-
-console.log(showTypeOf(money));
-console.log(showTypeOf(income));
-console.log(showTypeOf(deposit));
-
-money = +prompt("Ваш месячный доход?","300000");
-
-if(!money){
-    confirm.log("Некорректное значение!");
-    money = +prompt("Ваш месячный доход?");
-}
-
-addExpenses = prompt("Перечислите возможные расходы за рассчитываемый период через запятую", "бензин,еда,комунальные платежи");
-
-deposit = confirm("Есть ли у вас депозит в банке?");
-expenses1 = prompt("Введите обязательную статью расходов?","еда");
-amount1 = +prompt("Во сколько это обойдется?", "10000");
-
-expenses2 = prompt("Введите обязательную статью расходов?", "комунальные платежи");
-amount2 = +prompt("Во сколько это обойдется?", "10000");
-
-
-let getExpensesMonth = function (amount1, amount2) {
-
-  return amount1+amount2;
-  
-}
-
-let getAccumulatedMonth = function (money, сallback) {
-
-  return money - сallback(amount1, amount2);
-  
-}
-
-accumulatedMonth = getAccumulatedMonth(money, getExpensesMonth);
-
-
-let getTargetMonth = function (mission, budgetMonth) {
-
-  return Math.ceil(mission / budgetMonth)
-  
-}
+let data="Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui. Etiam rhoncus. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum. Nam quam nunc, blandit vel, luctus pulvinar, hendrerit id, lorem. Maecenas nec odio et ante tincidunt tempus. Donec vitae sapien ut libero venenatis faucibus. Nullam quis ante. Etiam sit amet orci eget eros faucibus tincidunt. Duis leo. Sed fringilla mauris sit amet nibh. Donec sodales sagittis magna. Sed consequat, leo eget bibendum sodales, augue velit cursus nunc, Sed consequat, leo eget bibendum sodales, augue velit cursus nunc, Sed consequat, leo eget bibendum sodales, augue velit cursus nunc,!!!!!!!!!!!!!";
 
 
 
+let Exerpt = function(text){
+  if(typeof(text)!=="string"){
+    alert("аргумент не является строкой!");
+  }
+  text= text.trim();
 
-
-//console.log("Период равен " + period + " месяцев" );
-
-//console.log("Цель заработать " + mission + " рублей");
-console.log("Расходы за месяц "+getExpensesMonth(amount1, amount2));
-
-console.log(addExpenses.toLowerCase().split(', '));
-
-console.log("Цель будет достигнута за: "+ getTargetMonth(mission, accumulatedMonth)+" месяцев");
-
-//console.log("Бюджет на месяц "+accumulatedMonth);
-
-//console.log(addExpenses.length);
-
-
-
-let budgetDay = accumulatedMonth / 30;
-
-console.log("Бюджет на день "+ (Math.floor(budgetDay)));
-
-let getStatusIncome = function(budgetDay){
-
-  if(1200<=budgetDay){
-      return ("У вас высокий уровень дохода");
-
-  }else if(budgetDay>600&&budgetDay<1200){
-     return ("У вас средний уровень дохода");
-
-  }else if(budgetDay<=600){
-      return ("К сожалению у вас уровень дохода ниже среднего");
-
-  }else{
-      return ("Что то пошло не так");
+  if(text.length>30){
+    text = text.substring(0, 30);
+    text+="..."
   }
 
-
+    return text;
 }
 
-console.log(getStatusIncome(budgetDay));
+console.log(Exerpt(data))
